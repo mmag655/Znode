8588f023b1d5b7b0f1145b957bb0328e1194a268
Riaz Syed <ceo@futureit.ch>
2025-05-16 18:59:11 +0500

    fix: transaction approave

--- backend/controllers/api/transaction.py
+++ backend/controllers/api/transaction.py
@@ -47,32 +47,37 @@ def read_admin_transactions(user_id: int = Depends(get_current_user_id), db: Ses
         
     except Exception as e:
        return error_response(str(e), status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 @router.patch("/admin/approve/")
 async def approve_transaction(request: Request, db: Session = Depends(get_db), user_id: int = Depends(get_current_user_id)):
     try:
-        # check if user is admin
+        # Parse request body
         raw_body = await request.body()
         body = json.loads(raw_body.decode("utf-8"))
+        transaction_ids = body.get("transaction_ids", [])
+
+        # Validate admin role
         user = user_crud.get_user(db, user_id)
-        if not user.role == "admin":
+        if not user or user.role != "admin":
             return error_response("Unauthorized", status.HTTP_403_FORBIDDEN)
+
+        approved_transactions = []
         
-        # Approve transaction
-        print("transaction_ids : ", body)
-        for transaction_id in body["transaction_ids"]:
+        for transaction_id in transaction_ids:
             transaction = crud_transaction.get_transaction(db, transaction_id)
             if not transaction:
-                return error_response("Transaction not found", status.HTTP_404_NOT_FOUND)
-            
-            # Check if the transaction is already approved
+                return error_response(f"Transaction ID {transaction_id} not found", status.HTTP_404_NOT_FOUND)
+
             if transaction.transaction_status == "approved":
-                return error_response("Transaction already approved", status.HTTP_400_BAD_REQUEST)
-            
-            # Approve the transaction
-        transaction = crud_transaction.approve_transaction(db, transaction_id)
-        if not transaction:
-            return error_response("Transaction not found", status.HTTP_404_NOT_FOUND)
-        
-        return success_response(transaction.to_dict(), "Transaction approved")
+                return error_response(f"Transaction ID {transaction_id} already approved", status.HTTP_400_BAD_REQUEST)
+
+            # Approve transaction
+            approved = crud_transaction.approve_transaction(db, transaction_id)
+            if not approved:
+                return error_response(f"Transaction ID {transaction_id} could not be approved", status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+            approved_transactions.append(approved.to_dict())
+
+        return success_response(approved_transactions, "Transactions approved")
     except Exception as e:
-       return error_response(str(e), status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return error_response(str(e), status.HTTP_500_INTERNAL_SERVER_ERROR)
